787e8d87f418d639f177d582f947f140b4a9a342
HaeSeongHI <06shkim1@gmail.com>
2025-06-01 16:30:36 +0900

    일단 중간 체킹본(system pormpt 체킹 필요)

--- teamproject1.py
+++ teamproject1.py
@@ -6,13 +6,13 @@ text_ac_cont = text_ac.read_text(encoding='utf-8')
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key="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",
+  api_key="",
 )
 
-messages = [{'role':'system', 'content': 'You are an helpful, reasonable advisor'}]
+messages = [{'role':'system', 'content': 'You are an helpful, reasonable career counselor for university students. You would recommend to students about what subjects would be better for them to take in terms of their preference and talents.' + text_ac_cont}]
 
 while True:
-  msg = input('Are you curious about your future? I will tell you what to do ')
+  msg = input('Are you curious about your future? I will tell you what to do: ')
   messages.append({'role':'user', 'content':msg})
   completion = client.chat.completions.create(
 
